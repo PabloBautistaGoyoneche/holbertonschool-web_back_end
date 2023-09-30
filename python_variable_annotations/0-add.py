55cd552a2bb64c3fd6232c577ee306060ad25f93
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-09-29 22:08:25 -0700

    Web back end projects

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""type-annotated function add a float a and
+a float b as arguments and returns sum as float"""
+
+
+def add(a: float, b: float) -> float:
+    "Function sum two floats"
+    result = a + b
+    return result
