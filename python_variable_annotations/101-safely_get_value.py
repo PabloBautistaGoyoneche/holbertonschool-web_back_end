c92b978e12c668cda194a1fdcaacb588ce4b2c53
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-09-29 23:15:48 -0700

    Fix: Pycodestyle

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -7,7 +7,7 @@ T = TypeVar('T')
 
 
 def safely_get_value(dct: Mapping, key: Any,
-        default: Union[T, None]) -> Union[Any, T]:
+                     default: Union[T, None]) -> Union[Any, T]:
     "Function return a value from a dictionary"
     if key in dct:
         return dct[key]
