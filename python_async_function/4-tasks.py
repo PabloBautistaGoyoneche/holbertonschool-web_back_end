2a373c9222c02daa2e664098cc5b345053cc8022
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-09-30 07:02:28 -0700

    Fix: Documentation

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python3
-import asyncio
-from typing import List
-"""
-Async function task_wait_n generates completion times
-for n concurrent tasks with a max delay.
+""" Async function task_wait_n creates a list of
+    completion times for concurrent tasks with a max delay.
 
 Parameters:
     - n (int): Number of concurrent tasks.
-    - max_delay (int): Maximum time delay (in seconds) for each task.
+    - max_delay (int): Maximum delay (in seconds) for each task.
+    The function uses task_wait_random from
+    '3-tasks' module to execute tasks asynchronously
+    and returns a list of completion times.
 
     Example Usage:
     result_times = await task_wait_n(5, 3)
     # Executes 5 tasks with a max 3-second delay.
     # result_times contains completion times.
+
+    Note:
+    Ensure the '3-tasks' module with task_wait_random
+    is properly defined for this script.
 """
+import asyncio
+from typing import List
+
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
