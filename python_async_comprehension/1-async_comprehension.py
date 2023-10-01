306dd560b4975161deab8dfda590099e07657715
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-10-01 07:21:27 -0700

    Async Comprehensions

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+coroutine will collect 10 random numbers using
+an async comprehension over async_generator, then return the
+10 random numbers.
+"""
+
+from typing import List
+import random
+
+
+async def async_generator() -> List[float]:
+    """Async Generator that yields random floats"""
+    for _ in range(10):
+        yield random.uniform(0, 1)
+
+
+    async def async_comprehension() -> List[float]:
+        """Async Comprehensions"""
+        return [num async for num in async_generator()]
