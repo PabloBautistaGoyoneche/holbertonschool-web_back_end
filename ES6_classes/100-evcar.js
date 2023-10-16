49c9e7fe8c2908a938361b7f18c657f12c77ecfc
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-10-15 22:55:51 -0500

    Reorganizing the code

--- ES6_classes/100-evcar.js
+++ ES6_classes/100-evcar.js
@@ -1,15 +1,15 @@
-//
 import Car from './10-car';
 
-class EVCar extends Car {
+export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
 
-  static get [Symbol.species]() {
-    return Car;
+  cloneCar() {
+    const clone = new (
+      Object.getPrototypeOf(this.constructor))(this._brand, this._motor, this._color);
+    clone._range = this._range;
+    return clone;
   }
 }
-
-export default EVCar;
