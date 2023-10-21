e75afb934b0e4fcc19cc2006ba71da2bec9fe7ad
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-10-21 08:40:58 -0500

    Parameter defaults

---
+++ ES6_basic/3-default-parameter.js
@@ -0,0 +1,5 @@
+// Define a function named getSumOfHoods with three parameters.
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  // Calculate the sum
+  return initialNumber + expansion1989 + expansion2019;
+}
