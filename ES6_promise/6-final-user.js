9a3c40e822f60256a0c062d75767014b07fc42fa
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-11-10 18:58:40 -0500

    The handleProfileSignup function handles user creation and photo uploads asynchronously, returning an array with the results of fulfilled or rejected promises.

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,9 +1,30 @@
-// Handle multiple promises
+// Import the 'signUpUser' and 'uploadPhoto' functions from external modules.
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
-}
+// Define an asynchronous function 'handleProfileSignup' that takes three parameters.
+const handleProfileSignup = async (firstName, lastName, fileName) => {
+  // Create an empty array 'result' to store the outcome of the operations.
+  const result = [];
+  try {
+    // Attempt to sign up a user with the provided 'firstName' and 'lastName'.
+    const user = await signUpUser(firstName, lastName);
+    // If successful, add the user's information to 'result' as a fulfilled promise.
+    result.push({ status: 'fulfilled', value: user });
 
+    // Upload a photo using the 'fileName'.
+    await uploadPhoto(fileName);
+  } catch (error) {
+    // If there is an error, catch it and add it to 'result' as a rejected promise
+    // with the error message.
+    result.push({
+      status: 'rejected',
+      value: error.toString(),
+    });
+  }
+  // Return the 'result' array, which contains the outcomes of the user signup and photo upload.
+  return result;
+};
+
+// Export the 'handleProfileSignup' function as the default export of this module.
 export default handleProfileSignup;
